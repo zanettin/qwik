e562c0a791b7d2e34a811d55fb635b51e6e994f3
Alex Patterson <alex@codingcat.dev>
2022-04-21 17:04:29 -0400

    initial menu

--- docs/src/layouts/tutorial/tutorial-menu.ts
+++ docs/src/layouts/tutorial/tutorial-menu.ts
@@ -10,6 +10,30 @@ export const menu: TutorialMenu = [
         path: '/tutorial/introduction/basics',
       },
       {
+        title: 'Adding Data',
+        path: '/tutorial/introduction/adding-data',
+      },
+      {
+        title: 'Dynamic Attributes',
+        path: '/tutorial/introduction/dynamic-attributes',
+      },
+      {
+        title: 'Styling',
+        path: '/tutorial/introduction/styling',
+      },
+      {
+        title: 'Nested Components',
+        path: '/tutorial/introduction/nested-components',
+      },
+      {
+        title: 'HTML Tags',
+        path: '/tutorial/introduction/html-tags',
+      },
+      {
+        title: 'Making an App',
+        path: '/tutorial/introduction/making-an-app',
+      },
+      {
         title: 'Lazy Loading',
         path: '/tutorial/introduction/lazy-loading',
       },
@@ -21,6 +45,297 @@ export const menu: TutorialMenu = [
       {
         title: 'Assignments',
         path: '/tutorial/reactivity/assignments',
+      },
+      {
+        title: 'Declarations',
+        path: '/tutorial/reactivity/declarations',
+      },
+      {
+        title: 'Statements',
+        path: '/tutorial/reactivity/statements',
+      },
+      {
+        title: 'Updating Arrays and Objects',
+        path: '/tutorial/reactivity/updating-arrays-objects',
+      },
+    ],
+  },
+  {
+    title: 'Props',
+    items: [
+      {
+        title: 'Declaring Props',
+        path: '/tutorial/reactivity/declaring-props',
+      },
+      {
+        title: 'Default Values',
+        path: '/tutorial/reactivity/default-values',
+      },
+      {
+        title: 'Spread Props',
+        path: '/tutorial/reactivity/spread-props',
+      },
+    ],
+  },
+  {
+    title: 'Props',
+    items: [
+      {
+        title: 'Declaring Props',
+        path: '/tutorial/reactivity/declaring-props',
+      },
+      {
+        title: 'Default Values',
+        path: '/tutorial/reactivity/default-values',
+      },
+      {
+        title: 'Spread Props',
+        path: '/tutorial/reactivity/spread-props',
+      },
+    ],
+  },
+  {
+    title: 'Logic',
+    items: [
+      {
+        title: 'If Blocks',
+        path: '/tutorial/reactivity/if-blocks',
+      },
+      {
+        title: 'Else Blocks',
+        path: '/tutorial/reactivity/else-blocks',
+      },
+      {
+        title: 'Else-If Blocks',
+        path: '/tutorial/reactivity/else-if-blocks',
+      },
+      {
+        title: 'Each Blocks',
+        path: '/tutorial/reactivity/each-blocks',
+      },
+      {
+        title: 'Keyed Each Blocks',
+        path: '/tutorial/reactivity/keyed-each-blocks',
+      },
+      {
+        title: 'Await Blocks',
+        path: '/tutorial/reactivity/',
+      },
+    ],
+  },
+  {
+    title: 'Events',
+    items: [
+      {
+        title: 'DOM events',
+        path: '/tutorial/reactivity/dom-events',
+      },
+      {
+        title: 'Inline Handlers',
+        path: '/tutorial/reactivity/inline-handlers',
+      },
+      {
+        title: 'Event Modifiers',
+        path: '/tutorial/reactivity/event-modifiers',
+      },
+      {
+        title: 'Component Events',
+        path: '/tutorial/reactivity/component-events',
+      },
+      {
+        title: 'Event Forwarding',
+        path: '/tutorial/reactivity/event-forwarding',
+      },
+      {
+        title: 'DOM Event Forwarding',
+        path: '/tutorial/reactivity/dom-event-forwarding',
+      },
+    ],
+  },
+  {
+    title: 'Bindings',
+    items: [
+      {
+        title: 'Text Inputs',
+        path: '/tutorial/reactivity/text-inputs',
+      },
+      {
+        title: 'Numeric Inputs',
+        path: '/tutorial/reactivity/numeric-inputs',
+      },
+      {
+        title: 'Checkbox Inputs',
+        path: '/tutorial/reactivity/checkbox-inputs',
+      },
+      {
+        title: 'Group Inputs',
+        path: '/tutorial/reactivity/group-inputs',
+      },
+      {
+        title: 'Textarea Inputs',
+        path: '/tutorial/reactivity/textarea-inputs',
+      },
+      {
+        title: 'Select Bindings',
+        path: '/tutorial/reactivity/select-bindings',
+      },
+      {
+        title: 'Select Multiple',
+        path: '/tutorial/reactivity/select-multiple',
+      },
+      {
+        title: 'Content Editable Bindings',
+        path: '/tutorial/reactivity/content-editable-bindings',
+      },
+      {
+        title: 'Each Block Bindings',
+        path: '/tutorial/reactivity/each-block-bindings',
+      },
+      {
+        title: 'Media Elements',
+        path: '/tutorial/reactivity/media-elements',
+      },
+      {
+        title: 'Dimensions',
+        path: '/tutorial/reactivity/dimensions',
+      },
+      {
+        title: 'This',
+        path: '/tutorial/reactivity/this',
+      },
+      {
+        title: 'Component Bindings',
+        path: '/tutorial/reactivity/component-bindings',
+      },
+      {
+        title: 'Binding to Component Bindings',
+        path: '/tutorial/reactivity/binding-to-component-bindings',
+      },
+      {
+        title: 'Binding to Component Instances',
+        path: '/tutorial/reactivity/binding-to-component-instances',
+      },
+    ],
+  },
+  {
+    title: '',
+    items: [
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+    ],
+  },
+  {
+    title: '',
+    items: [
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+    ],
+  },
+  {
+    title: 'Lifecycle',
+    items: [
+      {
+        title: 'onMount',
+        path: '/tutorial/reactivity/onmount',
+      },
+      {
+        title: 'onWindow',
+        path: '/tutorial/reactivity/onwindow',
+      },
+      {
+        title: 'onDocument',
+        path: '/tutorial/reactivity/ondocument',
+      },
+    ],
+  },
+  {
+    title: '',
+    items: [
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+    ],
+  },
+  {
+    title: '',
+    items: [
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+    ],
+  },
+  {
+    title: '',
+    items: [
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
+      },
+      {
+        title: '',
+        path: '/tutorial/reactivity/',
       },
     ],
   },
